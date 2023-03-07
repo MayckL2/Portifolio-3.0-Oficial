19c13a527c8806af46ca4d2b76b9d1e0cb11c4c4
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-03-07 07:06:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let modal= document.querySelector(".modal")
 
+// abre modal
 function openModal(){
   modal.style.display="flex"
 }
 
+// fecha modal
 function closeModal(){
   modal.style.display="none"
 }
